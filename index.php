5a30df5c93d6ed4299cdab3986744818553153c3
kana-chan <bkm0115_y@icloud.com>
2021-03-01 15:41:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,26 @@
     <h1 class="title"> Leap Year </h1>
     <button>CSV出力</button>
     <?php
-      $leaps = array();
-      for ($y=1; $y<=100000; $y++){
+      $leaps = [];
+      for ($y=1; $y<=10; $y++){
+
         if ($y % 400 == 0) {
-          $leaps[] = array($y,true);
+          $leaps[] = [$y,1];
         } elseif ($y % 100 == 0) {
-          $lesps[] = array($y,false);
+          $leaps[] = [$y,0];
         } elseif ($y % 4 == 0 ) {
-          $leaps[] = array($y,true);
+          $leaps[] = [$y,1];
         } else {
-          $lesps[] = array($y,false);
+          $leaps[] = [$y,0];
         }
       }
 
+      var_dump ( $leaps );
       foreach($leaps as $l){
-        if ($l[1]) {
+        if ($l[1] == 1) {
           echo "${l[0]}年は、閏年です <br/>";
+        } else {
+          echo "${l[0]}.<br/>";
         }
       }
     ?>
