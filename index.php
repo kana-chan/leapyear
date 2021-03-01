f6da2be97da6a3b1246da547d1addb53ddf7e4a5
kana-chan <bkm0115_y@icloud.com>
2021-03-01 15:46:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,23 +3,22 @@
     <h1 class="title"> Leap Year </h1>
     <button>CSV出力</button>
     <?php
-    
-      $leaps = [];
-      for ($y=1; $y<=10; $y++){
-        if ($y % 400 == 0) {
-          $leaps[] = [$y,1];
-        } elseif ($y % 100 == 0) {
-          $leaps[] = [$y,0];
-        } elseif ($y % 4 == 0 ) {
-          $leaps[] = [$y,1];
-        } else {
-          $leaps[] = [$y,0];
+      $leaps = [];                                         //100000までをif文で閏年かどうかの判定をした上で、leaps配列に追加。
+      for ($y=1; $y<=1000; $y++){                            //0に年数、1に閏年かどうか
+        if ($y % 400 == 0) {                               //400の倍数は閏年
+          $leaps[] = [$y,true];
+        } elseif ($y % 100 == 0) {                         //100の倍数は平年
+          $leaps[] = [$y,false];
+        } elseif ($y % 4 == 0 ) {                          //4の倍数は閏年
+          $leaps[] = [$y,true];
+        } else {                                           //それ以外
+          $leaps[] = [$y,false];
         }
       }
 
       // var_dump ( $leaps );
       foreach($leaps as $l){
-        if ($l[1] == 1) {
+        if ($l[1] == true) {
           echo "${l[0]}年は、閏年です <br/>";
         } else {
           echo "${l[0]}.<br/>";
