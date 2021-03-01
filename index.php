ece53e98c1cc8740b05243b4a9a275bc69c8dea7
kana-chan <bkm0115_y@icloud.com>
2021-03-01 15:54:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,24 +4,25 @@
     <button>CSV出力</button>
     <?php
       $leaps = [];                                         //100000までをif文で閏年かどうかの判定をした上で、leaps配列に追加。
-      for ($y=1; $y<=1000; $y++){                            //0に年数、1に閏年かどうか、2に1年から数えて何回目か
+      $t = 0;                                              //何回目の閏年かを計算するための変数
+      for ($y=1; $y<=1000; $y++){                          //0に年数、1に閏年かどうか、2に1年から数えて何回目か
         if ($y % 400 == 0) {                               //400の倍数は閏年
-          $leaps[] = [$y,true];
+          $leaps[] = [$y,true,++$t];
         } elseif ($y % 100 == 0) {                         //100の倍数は平年
-          $leaps[] = [$y,false];
+          $leaps[] = [$y,false,"-"];
         } elseif ($y % 4 == 0 ) {                          //4の倍数は閏年
-          $leaps[] = [$y,true];
+          $leaps[] = [$y,true,++$t];
         } else {                                           //それ以外
-          $leaps[] = [$y,false];
+          $leaps[] = [$y,false,"-"];
         }
       }
 
       // var_dump ( $leaps );
       foreach($leaps as $l){
         if ($l[1] == true) {
-          echo "${l[0]}年は、${l[1]}回目の閏年です <br/>";
+          echo "${l[0]}年は、${l[2]}回目の閏年です <br/>";
         } else {
-          echo "${l[0]}.<br/>";
+          echo "${l[2]}.<br/>";
         }
       }
     ?>
