490588b7374f1e3151d0cfb5830bd2830dc3dcbd
kana-chan <bkm0115_y@icloud.com>
2021-03-01 15:06:04 +0900

    fizzbazz

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
     <h1 class="title"> Leap Year </h1>
     <button>CSV出力</button>
     <?php
-      for ($y=1; $y<=10; $y++){
-        echo $y
+      for ($y=1; $y<=1000; $y++){
+        if ($y % 400 == 0) {
+          echo "{$y}は閏年";
+        } elseif ($y % 100 == 0) {
+          echo " ";
+        } elseif ($y % 4 == 0 ) {
+          echo "{$y}は閏年";
+        } else {
+          echo " ";
+        }
       }
     ?>
   </body>
