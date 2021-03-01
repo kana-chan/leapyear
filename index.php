f78a9fc9e2baf5c2d43bab3dd185271de398e786
kana-chan <bkm0115_y@icloud.com>
2021-03-01 17:02:29 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,43 +6,43 @@
     <?php
       function createCsv ($data,$name) {
         $file = fopen($name,"w");
-        fputcsv($file,$data);
+        foreach ($data as $datum) { 
+            fputcsv($file, $datum);
+          };
         fclose($file);
       };
 
       $leaps = [];                                         //100000までをif文で閏年かどうかの判定をした上で、連想配列leapsに追加。
       $t = 0;                                              //何回目の閏年かを計算するための変数
-      for ($y=1; $y<=1000; $y++){                          //[year]に年数、[isLeap]に閏年かどうか、[times]に何回目の閏年か
+      for ($y=1; $y<=1000; $y++){                          //[year]に年数、[isLeap]に閏年どうか、[times]に何回目の閏年か
         if ($y % 400 == 0) {                               //400の倍数は閏年
           $leaps[] = [
             "year" => $y,
-            "isLeap" => true,
+            "isLeap" => 1,
             "times" => ++$t
           ];
         } elseif ($y % 100 == 0) {                         //100の倍数は平年
           $leaps[] = [
             "year" => $y,
-            "isLeap" => false,
+            "isLeap" => 0,
             "times" => 0
           ];
         } elseif ($y % 4 == 0 ) {                          //4の倍数は閏年
           $leaps[] = [
             "year" => $y,
-            "isLeap" => true,
+            "isLeap" => 1,
             "times" => ++$t
           ];
         } else {                                           //それ以外
           $leaps[] = [
             "year" => $y,
-            "isLeap" => false,
+            "isLeap" => 0,
             "times" => 0
           ];
         }
       }
 
-
-
-
+      createCsv($leaps,"test.csv")
 
       // var_dump ( $leaps );
       // foreach($leaps as $l){
